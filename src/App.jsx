bdda66a732120b08c49347c75018eb21e388e871
Khanhbroo <khanhmathias532211@gmail.com>
2025-05-11 17:50:38 +0700

    Adding a "MealItem" Component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Meal from "./components/Meal";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>
+        <Meal />
+      </div>
+    </>
+  );
+}
+
+export default App;
